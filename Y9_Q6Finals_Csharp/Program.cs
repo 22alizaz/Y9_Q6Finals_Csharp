4289baf63fbfe2c8eaac8d18ead579e97df59b27
Aliza Zubair <22AZubair@heckgrammar.co.uk>
2025-06-28 10:42:48 +0100

    Add project files.

---
+++ Y9_Q6Finals_Csharp/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Y9_Q6Finals_Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string user1 = "Yusuf5";
+            string user2 = "Mary80";
+            string pass1 = "33kk";
+            string pass2 = "af5r";
+            bool access = false;
+            while (access == false)
+            {
+                Console.WriteLine("Enter username");
+                string username = Console.ReadLine();
+                Console.WriteLine("Enter password");
+                string password = Console.ReadLine();
+                if (username == user1 && password == pass1)
+                {
+                    Console.WriteLine("Access granted");
+                    access = true;
+                }
+                else if (username == user2 && password == pass2)
+                {
+                    Console.WriteLine("Access granted");
+                    access = true;
+                } else
+                {
+                    Console.WriteLine("Access denied");
+                }
+                    
+            }
+        }
+    }
+}
